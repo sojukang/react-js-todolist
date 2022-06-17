03efed628234804be2fb9068c68be9ac1c533b84
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-06-17 22:37:35 +0900

    feat: TodoList 컴포넌트 생성

--- js-todolist/src/App.js
+++ js-todolist/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createGlobalStyle} from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -16,6 +17,7 @@ function App() {
             <GlobalStyle/>
             <TodoTemplate>
                 <TodoHead />
+                <TodoList />
             </TodoTemplate>
         </>
     );
